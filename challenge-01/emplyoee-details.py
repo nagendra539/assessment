29309dec07bb819beef3f8cdf48c8781324ba059
Nagendra Chunduru <nagendra@Nagendras-MacBook-Air.local>
2024-09-05 12:46:59 +0530

    committed

--- challenge-01/emplyoee-details.py
+++ challenge-01/emplyoee-details.py
@@ -4,12 +4,16 @@ Employees = pd.read_excel("table_data.xlsx", sheet_name="Employees")
 Devices = pd.read_excel("table_data.xlsx", sheet_name="Devices")
 print(Employees)
 print(Devices)
+common = []
 
 for id in Employees['id']:
     for empid in Devices['employee_id']:
-        if id != empid:
-            print(id)
-            break
+        if id == empid:
+            common.append[id]
+
+print common
+            
+            
         
 
 
