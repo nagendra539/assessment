75cf12e44de1f0fee5a0543e36cbff9f00200bf4
Nagendra Chunduru <nagendra@Nagendras-MacBook-Air.local>
2024-09-05 12:22:16 +0530

    committed

--- challenge-01/emplyoee-details.py
+++ challenge-01/emplyoee-details.py
@@ -5,6 +5,6 @@ Devices = pd.read_excel("table_data.xlsx", sheet_name="Devices")
 print(Employees)
 print(Devices)
 
-Emp_Dev = pd.merge(Employees, Devices, on='employee_id', how='left', indicator=True)
+Emp_Dev = pd.merge(Devices, Employees, on='employee_id', how='left', indicator=True)
 
 print(Emp_Dev)
