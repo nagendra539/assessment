09d7e9854c760a7465a062d13ae05a812f8f83eb
Nagendra Chunduru <nagendra@Nagendras-MacBook-Air.local>
2024-09-05 13:35:02 +0530

    committed

--- challenge-01/emplyoee-details.py
+++ challenge-01/emplyoee-details.py
@@ -10,22 +10,5 @@ for id in Employees['id']:
     for empid in Devices['employee_id']:
         if id == empid:
             common1.append(id)
-
-
+            
 print(Employees[~Employees['id'].isin(common1)])
- #print(filtered_df)
-
-# not_in_column = [item for item in Employees['id'].values if item not in common1]
-# print(not_in_column)
-
-# for column in Employees.columns[]:
-#     not_in_list = Employees[~Employees[column].isin(common1)][column]
-#     print(f"Elements in column '{column}' not in list:")
-#     print(not_in_list.values)
-#     print('-' * 50)
-
-            
-            
-        
-
-
