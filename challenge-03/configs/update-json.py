89bd313e39bcc2f0a4f668e583c385253528c1a4
Nagendra Chunduru <nagendra@Nagendras-MacBook-Air.local>
2024-09-05 17:53:41 +0530

    committed

--- challenge-03/configs/update-json.py
+++ challenge-03/configs/update-json.py
@@ -18,30 +18,30 @@ def update_json(csv_file, json_file, env):
             }
     print (data_to_update)
 
-    # Read the JSON file
-    with open(json_file, 'r') as file:
-        json_data = json.load(file)
+#     # Read the JSON file
+#     with open(json_file, 'r') as file:
+#         json_data = json.load(file)
 
-    # Update only the part of the JSON file related to the given environment
-    if env in json_data:
-        json_data[env].update(data_to_update)
-    else:
-        print(f"Environment {env} not found in JSON file.")
-        return
+#     # Update only the part of the JSON file related to the given environment
+#     if env in json_data:
+#         json_data[env].update(data_to_update)
+#     else:
+#         print(f"Environment {env} not found in JSON file.")
+#         return
 
-    # Write the updated data back to the JSON file
-    with open(json_file, 'w') as file:
-        json.dump(json_data, file, indent=4)
+#     # Write the updated data back to the JSON file
+#     with open(json_file, 'w') as file:
+#         json.dump(json_data, file, indent=4)
 
-    print(f"JSON file updated successfully for environment: {env}")
+#     print(f"JSON file updated successfully for environment: {env}")
 
-if __name__ == "__main__":
-    # Ensure that the correct number of arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <csv_file> <json_file> <env>")
-        sys.exit(1)
+# if __name__ == "__main__":
+#     # Ensure that the correct number of arguments are provided
+#     if len(sys.argv) != 4:
+#         print("Usage: python script.py <csv_file> <json_file> <env>")
+#         sys.exit(1)
 
-    csv_file = sys.argv[1]
-    json_file = sys.argv[2]
-    env = sys.argv[3]
+#     csv_file = sys.argv[1]
+#     json_file = sys.argv[2]
+#     env = sys.argv[3]
 update_json(csv_file, json_file, env)
