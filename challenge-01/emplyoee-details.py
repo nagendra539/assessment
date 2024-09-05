59c6841a63ae12836f28b0a9932e17ea35beeb23
Nagendra Chunduru <nagendra@Nagendras-MacBook-Air.local>
2024-09-05 13:36:37 +0530

    committed

--- challenge-01/emplyoee-details.py
+++ challenge-01/emplyoee-details.py
@@ -2,13 +2,11 @@ import pandas as pd
 
 Employees = pd.read_excel("table_data.xlsx", sheet_name="Employees")
 Devices = pd.read_excel("table_data.xlsx", sheet_name="Devices")
-print(Employees)
-print(Devices)
 common1 = []
 
 for id in Employees['id']:
     for empid in Devices['employee_id']:
         if id == empid:
             common1.append(id)
-            
-print(Employees[~Employees['id'].isin(common1)])
+print("Employee names which are not using the Company device:")
+print(Employees[~Employees['id'].isin(common1)].['first_name'])
