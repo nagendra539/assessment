e1d9603e8be63ebf570134fc4d317fe0155aac24
Nagendra Chunduru <nagendra@Nagendras-MacBook-Air.local>
2024-09-05 12:20:34 +0530

    committed

--- challenge-01/emplyoee-details.py
+++ challenge-01/emplyoee-details.py
@@ -2,8 +2,9 @@ import pandas as pd
 
 Employees = pd.read_excel("table_data.xlsx", sheet_name="Employees")
 Devices = pd.read_excel("table_data.xlsx", sheet_name="Devices")
-print(Employees.index[0])
-print(Devices.index[0])
+print(Employees)
+print(Devices)
 
-for row in Employees:
-    print(row['id'], row['first_name'], row['last_name'])
+Emp_Dev = pd.merge(Employees, Devices, on='employee_id', how=left, indicator=True)
+
+print(Emp_Dev)
