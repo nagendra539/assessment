fee7da1e1a33b2eaa279e304acec4c21f9d1d361
Nagendra Chunduru <nagendra@Nagendras-MacBook-Air.local>
2024-09-05 12:57:03 +0530

    committed

--- challenge-01/emplyoee-details.py
+++ challenge-01/emplyoee-details.py
@@ -4,14 +4,20 @@ Employees = pd.read_excel("table_data.xlsx", sheet_name="Employees")
 Devices = pd.read_excel("table_data.xlsx", sheet_name="Devices")
 print(Employees)
 print(Devices)
-common = []
+common1 = []
 
 for id in Employees['id']:
     for empid in Devices['employee_id']:
         if id == empid:
-            common.append(id)
+            common1.append(id)
 
-print(common)
+print(common1)
+
+filtered_df = Employees[Employees['id'].isin(common1)]
+
+print(filtered_df)
+
+
             
             
         
