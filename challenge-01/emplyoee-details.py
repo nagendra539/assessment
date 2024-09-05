07b49486312a2fef8891124c09a930dae52d4f9f
Nagendra Chunduru <nagendra@Nagendras-MacBook-Air.local>
2024-09-05 12:09:48 +0530

    committed

--- challenge-01/emplyoee-details.py
+++ challenge-01/emplyoee-details.py
@@ -4,3 +4,7 @@ Employees = pd.read_excel("table_data.xlsx", sheet_name="Employees")
 Devices = pd.read_excel("table_data.xlsx", sheet_name="Devices")
 print(Employees.index[0])
 print(Devices.index[0])
+
+for row in Employees:
+    print(row[id], row[first_name], row[last_name])
+    
