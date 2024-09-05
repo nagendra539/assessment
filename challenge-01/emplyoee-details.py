d97ab5fe32f3b71416c955a5dccce0997e2dc2cd
Nagendra Chunduru <nagendra@Nagendras-MacBook-Air.local>
2024-09-05 12:05:10 +0530

    committed

--- challenge-01/emplyoee-details.py
+++ challenge-01/emplyoee-details.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 Employees = pd.read_excel("table_data.xlsx", sheet_name="Employees")
 Devices = pd.read_excel("table_data.xlsx", sheet_name="Devices")
-print(Employees)
-print(Devices)
+print(Employees[0])
+print(Devices[0])
