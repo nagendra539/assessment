44131babe137c6d8aea97abf6121cb9ef4d86f5a
Nagendra Chunduru <nagendra@Nagendras-MacBook-Air.local>
2024-09-05 12:33:13 +0530

    committed

--- challenge-01/emplyoee-details.py
+++ challenge-01/emplyoee-details.py
@@ -8,4 +8,4 @@ print(Devices)
 for id in Employees['id']:
     for empid in Devices['employee_id']:
         if id == empid:
-            print(id, empid "matched with:")
+            print(id, empid)
