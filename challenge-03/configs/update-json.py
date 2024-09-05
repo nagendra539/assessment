81d4a844fc3f31fe5de998e6ea3f0f2123745de7
Nagendra Chunduru <nagendra@Nagendras-MacBook-Air.local>
2024-09-05 17:59:32 +0530

    committed

--- challenge-03/configs/update-json.py
+++ challenge-03/configs/update-json.py
@@ -23,12 +23,12 @@ def update_json(csv_file, json_file, env):
         json_data = json.load(file)
     print (json_data)
 
-#     # Update only the part of the JSON file related to the given environment
-#     if env in json_data:
-#         json_data[env].update(data_to_update)
-#     else:
-#         print(f"Environment {env} not found in JSON file.")
-#         return
+    # Update only the part of the JSON file related to the given environment
+    if env in json_data:
+        json_data[env].update(data_to_update)
+    else:
+        print(f"Environment {env} not found in JSON file.")
+        return
 
 #     # Write the updated data back to the JSON file
 #     with open(json_file, 'w') as file:
